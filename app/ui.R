8b84821a41fbbc8961bff61aeabeacd95cacda1c
Gaurav Shegokar <gaurao2009@gmail.com>
2016-12-09 21:32:34 +0530

    help doc added

--- app/ui.R
+++ app/ui.R
@@ -20,40 +20,46 @@ choices = c("weight" = "wt",
             "Transmission(0-auto | 1-manual)" = "am",
             "gear" = "gear",
             "carb" = "carb"
-            )
+)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("mtcars Dataset analysis"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("examples",
-                   "Number of Examples",
-                   min = 1,
-                   max = 32,
-                   value = 10),
-       radioButtons("radiobutton", "Select Feature", choices, selected = "wt", inline = FALSE,
-                    width = NULL),
-       tags$form(
-           checkboxGroupInput("checkItems", "Select Features for model building",
-                              choices),
-           actionButton("action1","Build Model")
-       )
-    ),
     
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotlyOutput("distPlot"),
-       verbatimTextOutput("event"),
-       br(),
-       h3("Model"),
-       br(),
-       verbatimTextOutput("event1")
+    # Application title
+    titlePanel("mtcars Dataset analysis"),
+    
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("examples",
+                        "Number of Examples",
+                        min = 1,
+                        max = 32,
+                        value = 10),
+            radioButtons("radiobutton", "Select Feature", choices, selected = "wt", inline = FALSE,
+                         width = NULL),
+            tags$form(
+                checkboxGroupInput("checkItems", "Select Features for model building",
+                                   choices),
+                actionButton("action1","Build Model")
+            )
+        ),
+        
+        # Show a plot of the generated distribution
+        mainPanel(
+            h2("Help"),
+            h4("1. You can select number of observations you want using slider"),
+            h4("2. You can choose the feature from radio button and the regression line will be drawn on the plot"),
+            h4("3. You can see p-values for perticular feature and adj. r squared value below plot"),
+            h4("4. You can select the features you want in model"),
+            h4("5. On clicking submit button the model will popup on the screen, here you can see the coefficients of each variable"),
+            plotlyOutput("distPlot"),
+            verbatimTextOutput("event"),
+            br(),
+            h3("Model"),
+            br(),
+            verbatimTextOutput("event1")
+        )
     )
-  )
-  
+    
 ))
